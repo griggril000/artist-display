a0ca8f4b9b5ec927f3940f39fc46a615a3415b7f
griggril000 <griggriley@gmail.com>
2017-05-11 20:00:45 -0600

    Update fusionad.js

--- artists/js/common/fusionad.js
+++ artists/js/common/fusionad.js
@@ -1,3 +1,4 @@
+/*
 (function(){
   var fusoionad_script = document.createElement('script');
   fusoionad_script.type = 'text/javascript';
@@ -6,3 +7,4 @@
   fusoionad_script.src = '//cdn.fusionads.net/fusion.js?zoneid=1332&serve=C6SDP2Y&placement=callmenickcom';
   document.body.appendChild(fusoionad_script);
 })();
+*/
